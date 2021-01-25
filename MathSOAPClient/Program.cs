d688f114cbba6e9a0b5268c9b437afb1eed48bbf
Tanay Gandhi <tgandhi1@sundevils.asu.edu>
2021-01-25 11:27:05 -0700

    Add project files.

---
+++ MathSOAPClient/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathSOAPClient.ServiceReference1;
+
+namespace MathSOAPClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Service1Client client = new Service1Client("BasicHttpBinding_IService1");
+
+            string userInput;
+            int result = 0;
+
+            DisplayMenuOptions();
+            userInput = Console.ReadLine();
+
+            while (!userInput.Equals("3"))
+            {
+                switch (userInput)
+                {
+                    case "1":
+                        Console.WriteLine("Enter an int number:");
+                        userInput = Console.ReadLine();
+
+                        try
+                        {
+                            result = client.SumOfDigits(int.Parse(userInput));
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Please enter a valid integer");
+                            result = -1;
+                        }
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Enter a string");
+                        userInput = Console.ReadLine();
+                        result = client.VowelCount(userInput);
+                        break;
+
+                    case "3":
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid input, try again");
+                        break;
+                }
+                Console.WriteLine("Result: {0}\n", result);
+                Console.WriteLine("Enter another option or type \'3\' to exit");
+                userInput = Console.ReadLine();
+            }
+
+        }
+
+        public static void DisplayMenuOptions()
+        {
+            Console.WriteLine("Welcome to Web Service Demo");
+            Console.WriteLine("Select the method you need to invoke");
+            Console.WriteLine("\t1. Sum of digits");
+            Console.WriteLine("\t2. Vowel Count");
+            Console.WriteLine("\t3. Exit\n");
+            Console.WriteLine("Enter your option: ...");
+        }
+    }
+}
---
+++ MathSOAPService/Service1.svc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MathSOAPService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public static List<char> vowels = new List<char>() { 'a', 'e', 'i', 'o', 'u' };
+        public int SumOfDigits(int num)
+        {
+            int sum = 0;
+            while (num != 0)
+            {
+                //Get last rightmost digit of num and divide it off
+                sum += num % 10;
+                num /= 10;
+            }
+            return sum;
+        }
+
+        public int VowelCount(string word)
+        {
+            int vowelCount = 0;
+            foreach (char letter in word)
+            {
+                if (vowels.Contains(letter))
+                {
+                    vowelCount++;
+                }
+            }
+            return vowelCount;
+        }
+    }
+}
